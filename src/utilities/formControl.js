0dc95be20246f6c3ccea98729af0951eea8a65cb
omer9977 <63466920+omer9977@users.noreply.github.com>
2022-02-04 17:28:58 +0300

    login2

--- src/utilities/formControl.js
+++ src/utilities/formControl.js
@@ -1,6 +1,7 @@
 import { useField } from 'formik';
 import React from 'react';
 import { FormField, Label } from 'semantic-ui-react';
+import '../css/Login.css'
 
 export default function TextInput({...props}) {
   
